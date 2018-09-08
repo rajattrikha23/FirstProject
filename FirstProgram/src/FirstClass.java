0ec444d42b28915cd1fedb8cfb7b6e81b84fd565
Rajat T <rajatt@gmail.com>
2018-09-08 17:16:36 +0530

    Commit my first project.

---
+++ FirstProgram/src/FirstClass.java
@@ -0,0 +1,20 @@
+
+public class FirstClass {
+
+	  public String myVar="instance variable";
+	    
+	   public void myMethod(){
+	    	// local variable
+	     String myVar = "Inside Method";
+	    	System.out.println(myVar);
+	   }
+	   public static void main(String args[]){
+	      // Creating object
+	      FirstClass obj = new FirstClass();
+	    	
+	    
+	      System.out.println("Calling Method");
+	      obj.myMethod();
+	      System.out.println(obj.myVar);
+	   }
+	}
